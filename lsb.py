a41153427cbe83fe7f5f07b5100a9262a708f8c7
Charaf-Br <57442661+Charaf-Br@users.noreply.github.com>
2023-12-12 19:13:15 +0100

    Update lsb.py

--- lsb.py
+++ lsb.py
@@ -5,19 +5,20 @@ from os import path
 import cv2
 import numpy as np
 
-class LSB():
-    BITS = 2
-
-    HIGH_BITS = 256 - (1 << BITS)
-    LOW_BITS = (1 << BITS) - 1
-    BYTES_PER_BYTE = math.ceil(8 / BITS)
-    FLAG = '%'
-
-    def insert(self,image_path, msg, encoded_path):
-        img = cv2.imread(image_path, cv2.IMREAD_ANYCOLOR)
+class Lsb:
+    def __init__(self,BITS) -> None:
+        
+        self.BITS = BITS
+        self.HIGH_BITS = 256 - (1 << BITS)
+        self.LOW_BITS = (1 << BITS) - 1
+        self.BYTES_PER_BYTE = math.ceil(8 / BITS)
+        self.FLAG = '%'
+    
+    def insert(self,img_path, msg) -> str:
+        img = cv2.imread(img_path, cv2.IMREAD_ANYCOLOR)
         # Save origin shape to restore image
         ori_shape = img.shape
-        print('original shape: ', ori_shape)
+        print('original shape: ',ori_shape)
         max_bytes = ori_shape[0] * ori_shape[1] // self.BYTES_PER_BYTE
         # Encode message with length
         msg = '{}{}{}'.format(len(msg), self.FLAG, msg)
@@ -25,30 +26,31 @@ class LSB():
             msg), "Message greater than capacity:{}".format(max_bytes)
         data = np.reshape(img, -1)
         for (idx, val) in enumerate(msg):
-            self.encode(data[idx * self.BYTES_PER_BYTE: (idx + 1) * self.BYTES_PER_BYTE], val)
+            self.encode(data[idx*self.BYTES_PER_BYTE: (idx+1) * self.BYTES_PER_BYTE], val)
 
         img = np.reshape(data, ori_shape)
-
-        cv2.imwrite(encoded_path, img)
-
-    def encode(self,block, data):
+        filename, _ = path.splitext(img_path)
+        filename += '_lsb_embeded' + ".png"
+        cv2.imwrite(filename, img)
+        return filename
+    
+    def encode(self,block, data) -> None:
         # returns the Unicode code from a given character
         data = ord(data)
         for idx in range(len(block)):
             block[idx] &= self.HIGH_BITS
             block[idx] |= (data >> (self.BITS * idx)) & self.LOW_BITS
-
-
-
-    def extract(self,image_path):
-        img = cv2.imread(image_path, cv2.IMREAD_ANYCOLOR)
+    
+    
+    def extract(self,path) -> str:
+        img = cv2.imread(path, cv2.IMREAD_ANYCOLOR)
         data = np.reshape(img, -1)
         total = data.shape[0]
         res = ''
         idx = 0
         # Decode message length
         while idx < total // self.BYTES_PER_BYTE:
-            ch = self.decode(data[idx * self.BYTES_PER_BYTE: (idx + 1) * self.BYTES_PER_BYTE])
+            ch = self.decode(data[idx*self.BYTES_PER_BYTE: (idx+1)* self.BYTES_PER_BYTE])
             idx += 1
             if ch == self.FLAG:
                 break
@@ -58,12 +60,21 @@ class LSB():
 
         secret = ''
         while idx < end:
-            secret += self.decode(data[idx * self.BYTES_PER_BYTE: (idx + 1) * self.BYTES_PER_BYTE])
+            secret += self.decode(data[idx* self.BYTES_PER_BYTE: (idx+1)* self.BYTES_PER_BYTE])
             idx += 1
+            # print(secret)
         return secret
-
-    def decode(self,block):
+    
+    def decode(self,block) -> str:
         val = 0
         for idx in range(len(block)):
             val |= (block[idx] & self.LOW_BITS) << (idx * self.BITS)
         return chr(val)
+    
+    
+#determiner le nombre de bits sur lequel on va travailler
+lsb = Lsb(2)
+
+# print(lsb.insert('./assets/ubuntu.jpg', 'let\'s meet at 10:00 under the bridge'))
+
+# print(lsb.extract('./assets/ubuntu_lsb_embeded.png'))
